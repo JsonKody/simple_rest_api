8e99fe3a8ddd14cb7d46b6cb9d62398086ef10b7
JsonKody <daniel8cech@gmail.com>
2024-04-24 10:55:54 +0200

    saved by script

---
+++ elixir_phoenix/lib/elixir_phoenix/accounts/accounts.ex
@@ -0,0 +1,26 @@
+defmodule ElixirPhoenix.Accounts do
+  alias ElixirPhoenix.Repo
+  alias ElixirPhoenix.Accounts.User
+
+  def list_users do
+    Repo.all(User)
+  end
+
+  def get_user!(id), do: Repo.get!(User, id)
+
+  def create_user(attrs) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_user(%User{} = user, attrs) do
+    user
+    |> User.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_user(%User{} = user) do
+    Repo.delete(user)
+  end
+end
